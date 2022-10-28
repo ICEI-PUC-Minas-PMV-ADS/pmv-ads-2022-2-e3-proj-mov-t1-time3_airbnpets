858a83edcd807b55d0e33acb221a283ab3c56fa0
Arthur Rocha <artthurroccha@gmail.com>
2022-10-28 18:47:56 -0300

    criação-rota-e-esqueleto-cadastro-hotel

---
+++ src/airbnpets/src/Screens/CadastroHotel/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+export default function CadastroHotel() {
+  return (
+    <View>
+      <Text>Cadastro Hotel</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
